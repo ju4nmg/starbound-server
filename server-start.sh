a31d88bee40ccd90cc411fb1af9b12e6aa11c7bc
thrash3d <41959226+thrash3d@users.noreply.github.com>
2019-11-26 17:43:56 -0300

    Add files via upload

--- server-start.sh
+++ server-start.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 #Created by jmgimenez
+#user and password
+sed -i "s/USERNAME/${user}/g" ${STEAM}/starbound_update.exe
+sed -i "s/PASSWORD/${pass}/g" ${STEAM}/starbound_update.exe
+
 #update
-/home/steam/steamcmd.sh +runscript starbound_update.txt
+${STEAM}/steamcmd.sh +runscript starbound_update.txt
 
 #run starbound server
 cd ${STEAM}/starbound/linux
