30788918f90ab68b3ad1e6441c790aae7984ee4e
thrash3d <41959226+thrash3d@users.noreply.github.com>
2019-11-27 11:51:36 -0300

    Update server-start.sh

--- server-start.sh
+++ server-start.sh
@@ -2,12 +2,12 @@
 
 #Created by jmgimenez
 #user and password
-sed -i "s/USERNAME/${user}/g" /home/steam/steamcmd/starbound_update.exe
-sed -i "s/PASSWORD/${pass}/g" /home/steam/steamcmd/starbound_update.exe
+sed -i "s/USERNAME/${user}/g" steam/steamcmd/starbound_update.exe
+sed -i "s/PASSWORD/${pass}/g" /steam/steamcmd/starbound_update.exe
 
 #update
-/home/steam/steamcmd/steamcmd.sh +runscript starbound_update.txt
+/steam/steamcmd/steamcmd.sh +runscript starbound_update.txt
 
 #run starbound server
-cd /home/steam/steamcmd/starbound/linux
+cd /steam/steamcmd/starbound/linux
 ./starbound_server
